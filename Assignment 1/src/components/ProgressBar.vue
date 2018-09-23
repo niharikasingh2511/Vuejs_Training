b747bcd4dbd03cb967ff2f42a141ecd4565aaf87
Niharika Singh <niharika.singh@Niharikas-Mac.local>
2018-09-23 22:57:49 +0530

    Assignment 1 in progress

---
+++ Assignment 1/src/components/ProgressBar.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="progress text-center">
+        <div :class="['progress-bar', 'progress-bar-'+progressStatus]" :style="{ width: percentage + '%' }"></div>
+        <span v-if="percentage" class="percentage"> Your List is {{percentage}}% Full </span>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: ['list', 'total'],
+        computed: {
+            percentage: function () {
+                return Math.round((this.list.length / this.total) * 100);
+            },
+            progressStatus: function() {
+                const {percentage} = this;
+
+                if (percentage < 50) {
+                    return "success";
+                } else if (percentage >= 50 && percentage < 80) {
+                    return "warning";
+                } else if (percentage >= 80) {
+                    return "danger";
+                }
+            }
+        }
+    };
+</script>
+
+<style>
+    .percentage {
+        position: absolute;
+        width: 100%;
+        left: 0;
+    }
+    .progress{
+        height: 100px;
+    }
+</style>
